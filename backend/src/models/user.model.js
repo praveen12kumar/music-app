9e1a2d25a870c6672c4900ba414f9e98da13d93a
Praveen Kumar <praveenshakya441@gmail.com>
2025-01-15 16:59:34 +0530

    refactor user model

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -7,30 +7,35 @@ import { JWT_EXPIRY, JWT_SECRET } from "../config/config.js";
 const userSchema = new Schema({
     username:{
         type: String,
-        required: true,
+        required: [true, "can't be blank"],
+        lowercase: true,
+        match: [/^[a-zA-Z0-9._]+$/, "Username is invalid"],
         unique: true
     },
     email: {
         type: String,
         required: true,
-        unique: true
+        lowercase: true,
+        unique: [true, "can't be blank"],
+        match:[/\S+@\S+\.\S+/, 'is invalid'],
     },
 
     password:{
         type: String,
-        required: true
+        required: true,
+        minLength: [3, 'Password cannot be less than 3 characters']
     },
 
     likedSongs:{
-        type: String,
+        type: [String],
         default:""
     },
     subscribedArtists:{
-        type: String,
+        type: [String],
         default:""
     },
     subscribedPlaylists:{
-        type: String,
+        type: [String],
         default:""
     },
     profilePic:{
@@ -44,11 +49,15 @@ const userSchema = new Schema({
 
 
 userSchema.pre('save', async function(next){
-    const user = this;
-    const salt = await bcrypt.genSalt(10);
-    const encryptedPassword = await bcrypt.hash(user.password, salt);
-    user.password = encryptedPassword;
-    next();
+    if(!this.isModified('password')) return next();
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const encryptedPassword = await bcrypt.hash(this.password, salt);
+        this.password = encryptedPassword;
+        next();
+    } catch (error) {
+        next(error)
+    }
 })
 
 userSchema.methods.comparepassword = function compare(password){
--- backend/src/services/user-service.js
+++ backend/src/services/user-service.js
@@ -5,7 +5,6 @@ class UserService{
     constructor(){
         this.userRespository = new UserRepository();
     }
-
     async signup(data) {
         const user = await this.userRespository.create(data);
         return user;
